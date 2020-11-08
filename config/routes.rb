d5d20d1692d24cc4ad1230eb56179a4b11c9f2a7
yee-codes <shaoyee.ng@gmail.com>
2020-11-09 10:50:45 +1100

    still trying to fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :charges
   get '/about_us/index', to: 'about_us#index'
+  resources :opportunities
   # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
