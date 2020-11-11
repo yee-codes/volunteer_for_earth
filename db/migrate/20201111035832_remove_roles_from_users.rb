5322c56a2057f4d0dc7bed02c4d6c6ce23724fdb
yee-codes <shaoyee.ng@gmail.com>
2020-11-11 16:27:39 +1100

    redeploy

--- db/migrate/20201111035832_remove_roles_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveRolesFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :superadmin_role, :boolean
-    remove_column :users, :organisation_role, :boolean
-    remove_column :users, :user_role, :boolean
-  end
-end
